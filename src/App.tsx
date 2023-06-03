30e15012a3ec88a21505da6dd933c64ee4d27482
kiu_kokolishvili.ani <kokolishvili.ani@kiu.edu.ge>
2023-06-03 22:26:28 +0400

    added css-filter and connected Man-Model

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Car from './Cars';
 import Navbar from './Navbar';
+import OurRouter from './OurRouter';
 
 const App = () => {
 
   return (
     <div>
       <Navbar />
-      <Car />
+      {/* <Car /> */}
+      {/* <OurRouter /> */}
     </div>
   );
 }
