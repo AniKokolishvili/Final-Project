6ebd1475a300799037a63be7d14771510daaa821
kiu_kokolishvili.ani <kokolishvili.ani@kiu.edu.ge>
2023-06-05 20:02:55 +0400

    modified Cars, css, added CarsList

---
+++ README.txt
@@ -0,0 +1,11 @@
+fuel_type_id 2,3,6?
+gear_type_id 1,2,3?
+gear_type_id ???
+customs => განბაჟების საფასური
+location_id => which is which 
+parent_loc_id?
+
+
+
+order_date
+right_wheel
--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -1,70 +1,93 @@
-import React, { useEffect, useState } from 'react';
-
-type Option = {
-    label: string;
-    value: string;
-};
-
-// type DropdownProps = {
-//     options: Option[];
-// };
+// import React, { Component, useEffect, useMemo, useState } from 'react'
+// import { default as ReactSelect } from "react-select";
+// import { components } from "react-select";
 
 
 
-const Dropdown = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+// function Dropdown() {
+//     const [manufacturer, setManufacturer] = useState<any[]>([]);
 
-    const [manufacturer, setManufacturer] = useState<any[]>([]);
+//     const [isOpen, setIsOpen] = useState<boolean>(false)
 
-    //manufucturer
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch('https://static.my.ge/myauto/js/mans.json');
-            const jsonData = await response.json();
-            setManufacturer(jsonData);
-        };
-        fetchData();
-    }, []);
+//     const handleClick = () => {
+//         setIsOpen(!isOpen)
+//     }
 
-    const handleToggle = () => {
-        setIsOpen(!isOpen);
-    };
 
-    const handleOptionClick = (value: string) => {
-        if (selectedOptions.includes(value)) {
-            setSelectedOptions(selectedOptions.filter(option => option !== value));
-        } else {
-            setSelectedOptions([...selectedOptions, value]);
-        }
-    };
 
+//     //manufucturer
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             const response = await fetch('https://static.my.ge/myauto/js/mans.json');
+//             const jsonData = await response.json();
+//             setManufacturer(jsonData);
+//         };
+//         fetchData();
+//     }, []);
+
+//     let Drop;
+//     useMemo(() => {
+//         if (isOpen) {
+//             const Option = (props: { label: 'type' }) => {
+//                 return (<components.Option {...props}>
+//                     <div>
+//                         {manufacturer && manufacturer.map((item) => {
+//                             if (item.is_car === "1") {
+//                                 return <label key={item.man_id}>
+//                                     <input type="checkbox"
+//                                     />
+//                                     {item.man_name}
+//                                 </label>
+//                             }
+//                         }
+//                         )}
+//                     </div>
+
+//                 </components.Option>)
+//             }
+
+//         }
+//     }, [isOpen])
+
+//     return (
+//         <span
+//             data-toggle="popover"
+//             data-trigger="focus"
+//             data-content="Please selecet account(s)">
+//             {/* <button
+//                 area-aria-haspopup="true"
+//                 // disableElevation
+//                 // endIcon={}
+//                 onClick={() => handleClick()}
+//             >
+//                 {Drop || 'Select'}
+//             </button> */}
+//             <ReactSelect
+//                 options={manufacturer}
+//                 isMulti
+//                 closeMenuOnSelect={false}
+//                 hideSelectedOptions={false}
+//                 onChange={handleClick}
+//                 value={isOpen}
+//             />
+
+//         </span>
+//     )
+// }
+
+// export default Dropdown
+
+
+import React from 'react'
+
+function Dropdown() {
     return (
-        <div className="dropdown">
-            <div className="dropdown-toggle" onClick={() => handleToggle()}>
-                <select>
-                    {isOpen && (
-                        <option className="dropdown-menu">
-
-                            {manufacturer.map(item => (
-                                <label key={item.man_id} className="dropdown-option">
-                                    <input
-                                        type="checkbox"
-                                        value={item.man_id}
-                                        // checked={selectedOptions.includes(item.value)}
-                                        onChange={() => handleOptionClick(item.value)}
-                                    />
-                                    {item.man_name}
-                                </label>
-                            ))}
-
-                        </option>
-                    )}
-                </select>
-            </div>
+        <div>
 
         </div>
-    );
-};
+    )
+}
 
-export default Dropdown;
+export default Dropdown
+
+//queryparameter for url change
