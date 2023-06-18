7a5284ee0a74f02fff4c0fcee2d00da34155cf69
kiu_kokolishvili.ani <kokolishvili.ani@kiu.edu.ge>
2023-06-18 19:28:47 +0400

    first Final version

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-// import Dropdown from '@restart/ui/esm/Dropdown';
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import Car from './Cars';
+import Cars from './Cars';
 import Navbar from './Navbar';
-import OurRouter from './OurRouter';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// import Dropdown from './Dropdown';
 
 const App = () => {
 
   return (
     <div>
       <Navbar />
-      {/* <Car /> */}
-      {/* <OurRouter /> */}
-
-      {/* <Dropdown /> */}
-
+      <Cars />
     </div>
   );
 }
